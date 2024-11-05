72781cf0f5ec32f86fe3741cb75e1fe1ce283085
Evilen Barreto <159492801+evbarretoo@users.noreply.github.com>
2024-11-05 08:42:48 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Profile from './components/Profile/index'
 export default function App(){
   return(
     <div>
-      <Profile avatar={'./evbarretoo.jpg'} name={'Evilen Barreto'} bio={'Desenvolvedora de sistemas Python'} phone={'+5585999999999'} email={'evbarretoo@gmail.com'} githubUrl={'#'} linkedinUrl={'#'} twitterUrl={'#'}/>
+      <Profile avatar={'./evbarretoo.jpg'} name={'Evilen Barreto'} bio={'Desenvolvedora de sistemas python e javascript'} phone={'+5585999999999'} email={'evbarretoo@gmail.com'} githubUrl={'https://github.com/evbarretoo'} linkedinUrl={'#'} twitterUrl={'#'}/>
     </div>
   )
 }
